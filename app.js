1bec25c604ff5f41248c3fa226fca1355ea462bb
jagadeesh-431 <jagadeeshtmr43@gmail.com>
2025-03-19 19:25:36 +0530

    Add files via upload
    my first project using Java Script

---
+++ app.js
@@ -0,0 +1,10 @@
+var on_btn = document.getElementById("On");
+var off_btn = document.getElementById("Off");
+var img = document.getElementById("bulb");
+function bulb_On(){
+    img.src="https://i.pinimg.com/736x/8b/e0/60/8be0603fa9222908eac38746f7c72c2a.jpg";
+}
+function bulb_Off(){
+    img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu0oU1alfK3SfNP3oEihpTUlcFa35KqxjS6OAhy5zc9hPVuOabhKlSwY6sKDwBkojqIBA&usqp=CAU";
+    }
+    
